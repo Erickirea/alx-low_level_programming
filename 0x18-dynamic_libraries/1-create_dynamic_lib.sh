7fcc9d4c8043ce57896fe62fcbb25c39f943f1f1
Erickirea <ericfoxe98@gmail.com>
2023-12-11 13:00:16 -0800

    all

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -3,6 +3,6 @@
 # First we create object files for c files with the (-c -fPIC )
 # Then we create the dynamic library liball.so
 # Then we remove all the object files
-gcc -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedamtic  -c -fPIC *.c
 gcc -shared -o liball.so *.o
-rm *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
